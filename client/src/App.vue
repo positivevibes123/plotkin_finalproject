f9702396685346be86e64d3ec5a422f1f2889352
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-02-21 17:13:51 -0500

    Set up vue app
    Set up a basic vue app in the client folder, as well as package.json. Not functioning yet.

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+
+  // Add vue scripts here...
+
+  // Add bulma to this page using CDN. Should probably delete once Router gets involved
+
+
+</script>
+
+<template>
+  <!-- This code will have to be moved into it's own component. But for testing purposes it is here... -->
+   
+</template>
