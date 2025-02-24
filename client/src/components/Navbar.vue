2d163ce00d363a1140bdc9b9fd4bc71f0f63f4fa
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-02-24 13:40:17 -0500

    Made navbar component
    Made navbar into its own component so that does not need to be repeated in code.

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,19 +1,5 @@
-<script>
-
-  // Add vue scripts here...
-
-  // Add bulma to this page using CDN. Should probably delete once Router gets involved
-
-  export default{};
-
-
-</script>
-
 <template>
-  <!-- This code will have to be moved into it's own component. But for testing purposes it is here... -->
-  <!-- Code that would be in the html body tag goes here... -->
-   
-  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <nav class="navbar is-success" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
           <a class="navbar-item" href="index.html">
             <svg width="640" height="160" viewBox="0 0 640 160" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -82,85 +68,4 @@
           </div>
         </div>
       </nav>
-
-      <div class="container">
-        <div class="columns">
-            <div class="column is-invisible is-hidden-touch is-one-quarter">
-                <div class="box is-small-hidden"></div>
-            </div>
-            
-            <div class="column">
-                <div class="box has-text-success summary">
-                    <h2 class="title">Today</h2>
-                    <div class="columns is-multiline">
-                        <div class="column is-half">
-                            <h3 class="value">0 ft</h3>
-                            <caption class="caption">Distance</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">0:0</h3>
-                            <caption class="caption">Duration</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">NaN mph</h3>
-                            <caption class="caption">Avg Pace</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">0</h3>
-                            <caption class="caption">Calories</caption>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="box has-text-success summary">
-                    <h2 class="title">This week</h2>
-                    <div class="columns is-multiline">
-                        <div class="column is-half">
-                            <h3 class="value">0 ft</h3>
-                            <caption class="caption">Distance</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">0:0</h3>
-                            <caption class="caption">Duration</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">NaN mph</h3>
-                            <caption class="caption">Avg Pace</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">0</h3>
-                            <caption class="caption">Calories</caption>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="box has-text-success summary">
-                    <h2 class="title">All time</h2>
-                    <div class="columns is-multiline">
-                        <div class="column is-half">
-                            <h3 class="value">3.8 mi</h3>
-                            <caption class="caption">Distance</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">2:45</h3>
-                            <caption class="caption">Duration</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">1.4 mph</h3>
-                            <caption class="caption">Avg Pace</caption>
-                        </div>
-                        <div class="column is-half">
-                            <h3 class="value">1127.5</h3>
-                            <caption class="caption">Calories</caption>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="column is-invisible is-one-quarter">
-                <div class="box"></div>
-            </div> >
-        </div>
-    </div>
-
 </template>
