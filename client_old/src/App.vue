efa6d5a352e71939dcc338be85e99bbb54004c92
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-02-27 14:09:41 -0500

    Re-added Vue
    Re-added Vue with additional packages that Plotkin wanted to use. Old code moved to client_old, and will have to be transferred over to this new client and folder. The old client folder will then be deleted.

---
+++ client_old/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Home from './pages/Home.vue'
+import NavBar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    NavBar
+  }
+}
+</script>
+
+<template>
+  <NavBar/>
+  <Home/>
+</template>
