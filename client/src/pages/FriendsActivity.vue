088cd7dd6e3c9b7d602b818749cea83b7a75355c
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-03-02 15:54:18 -0500

    Started work on activity page, router work
    Added script to activity page which adds JSON data to array. This will hopefully be used to display activity content to page. Also added router functionality to some buttons.

---
+++ client/src/pages/FriendsActivity.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    type activityFormat = {
+        name: string;
+        description: string;
+        date: string;
+        time: string;
+        location: string;
+    };
+
+    // We must definte a format here, because if ref sees the array is empty (no params) it assumes it should ALWAYS be empty
+    const data = ref<activityFormat[]>([]);
+    
+    const addActivity = () => {
+        const activityObject = {
+        name: 'Activity Name',
+        description: 'Activity Description',
+        date: 'Activity Date',
+        time: 'Activity Time',
+        location: 'Activity Location'
+    };
+        
+        data.value.push(activityObject);
+        console.log(data.value[data.value.length - 1])
+    }
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="title">Friends Activity</h1>
+        <div class="columns">
+            <div class="column is-half is-offset-one-quarter">
+                <button @click="addActivity" class="button is-info is-fullwidth">Add Activity</button>
+                <br>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
+import FriendsActivity from '../pages/FriendsActivity.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/friends-activity', component: FriendsActivity },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    }
-  ],
+  routes
 })
 
 export default router
