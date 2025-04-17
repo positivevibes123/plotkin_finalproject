e2bc7c5b1bbf5cdd0d5c320b705cf7bd638262d0
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-04-17 15:18:44 -0400

    Added CORS header
    See title

--- server/index.js
+++ server/index.js
@@ -20,6 +20,16 @@ app
   })
   .use('/api/v1/users', usersController)
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+
 //error handling middleware
 app.use((err, req, res, next) => {
     console.error(err)
